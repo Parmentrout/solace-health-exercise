46787e90d2e0a8c9ddba334514a753d831413c72
Patrick Armentrout <patrick.armentrout@gmail.com>
2025-10-22 21:24:18 -0600

    Remove console log, add spacing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,6 @@ export default function Home() {
     console.log("fetching advocates...");
     fetch("/api/advocates").then((response) => {
       response.json().then((jsonResponse) => {
-        console.log(jsonResponse);
         setAdvocates(jsonResponse.data);
         setFilteredAdvocates(jsonResponse.data);
       });
